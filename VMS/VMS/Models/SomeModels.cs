2b1daa7db34c3dc278dde218756c18642e42ddfb
Антон Антоненко <toxaantonenko@mail.ru>
2019-07-29 13:00:46 +0300

    Add MVVM

---
+++ VMS/VMS/Models/SomeModels.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace VMS.Models
+{
+    class SomeModel : INotifyPropertyChanged // Просто пример
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
---
+++ VMS/VMS/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace VMS.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged // Вьюмодель для окна MainWindow
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
