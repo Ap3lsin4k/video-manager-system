da4f7ddfe8d76f1a142546cb99d2e3937ba811a0
Антон Антоненко <toxaantonenko@mail.ru>
2019-07-29 14:30:20 +0300

    Grid update

--- VMS/VMS/View/MainWindow.xaml.cs
+++ VMS/VMS/View/MainWindow.xaml.cs
@@ -20,9 +20,30 @@ namespace VMS
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int maxCamNums = 16;
+
+        int ColumRows = (int)Math.Sqrt(maxCamNums);
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ColumnDefinition[] ColDefs = new ColumnDefinition[ColumRows]; // Массив столбцов
+            RowDefinition[] RowDefs = new RowDefinition[ColumRows]; // Массив строчек
+
+            for (int i = 0; i < ColumRows; i++) 
+            {
+                ColDefs[i] = new ColumnDefinition(); // инициализация столбцов
+                MainGrid.ColumnDefinitions.Add(ColDefs[i]); // добавляем их в основную сетку
+
+                RowDefs[i] = new RowDefinition(); // инициализация строчек
+                MainGrid.RowDefinitions.Add(RowDefs[i]); // добавляем их в сетку
+            }
+
+           
+
+
+
         }
     }
 }
